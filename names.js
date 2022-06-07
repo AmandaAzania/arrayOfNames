e921e9fac8b39f3458abe08fa4bc706378879ceb
AmandaAzania <mandygudlwa@gmail.com>
2022-06-07 10:03:28 +0200

    first commit

---
+++ names.js
@@ -0,0 +1,8 @@
+let bestFriends = ['Nosipho', 'Simnikiwe', 'Joshe', 'Buli', 'Emile' , 'camaeron', 'Mikhail', 'Joel', 'clayton', 'Cassidy']
+
+const last = bestFriends[bestFriends.length - 1];
+
+const middle = bestFriends[bestFriends.length /2];
+console.log(last);
+
+console.log(middle);
